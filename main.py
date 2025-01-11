50dd83914eb5bf5e49026ebeae6205a9f4d2b449
juan <juanvalen15@gmail.com>
2025-01-11 20:45:36 +0100

    while loop

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 user_prompt = "Enter todo:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3, "Hello"]
-print(todos)
+todos = []
 
-print(type(todos))
+while True:
+    todo = input(user_prompt)
+    todos.append(todo.capitalize())
+    print(todos)
+
