69d6e40ae01703f8372b6769ebf7fddfeebad916
juan <juanvalen15@gmail.com>
2025-01-12 14:51:34 +0100

    added whatever case for incorrect commands

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-user_prompt = "Type add or show:"
+user_prompt = "Type add, show, or exit:"
 todo_prompt = "Enter a todo:"
 todos = []
 
 while True:
     user_action = input(user_prompt)
+    user_action = user_action.strip()
 
     match user_action:
         case 'add':
             todo = input(todo_prompt)
             todos.append(todo)
         case 'show':
-            print(todos)
+            for item in todos:
+                print(item)
         case 'exit':
             break
+        case whatever:
+            print("Incorrect command!")
 
 print('Bye!')
 
