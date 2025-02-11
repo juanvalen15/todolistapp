b1ad3b2814bc10878b662785d65110ff20b5e76c
juan <juanvalen15@gmail.com>
2025-02-11 17:28:29 +0100

    updated complete action to read and write to the todos.txt file

--- main.py
+++ main.py
@@ -28,13 +28,32 @@ while True:
                 print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
-            existing_todo = todos[number]
+            number = number - 1
+
+            with open('files/todos.txt','r') as file:
+                todos = file.readlines()
 
             new_todo = input(todo_prompt)
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            with open('files/todos.txt','w') as file:
+                file.writelines(todos)
+
         case 'complete':
             number = int(input("Number of the todo to : "))
-            todos.pop(number)
+            index  = number - 1
+
+            with open('files/todos.txt','r') as file:
+                todos = file.readlines()
+
+            todo_to_remove = todos[index]
+            todos.pop(index)
+
+            with open('files/todos.txt','w') as file:
+                file.writelines(todos)
+
+            print(f"Todo {todo_to_remove.strip('\n')} was removed from the list.")
+
         case 'exit':
             break
         case _:
