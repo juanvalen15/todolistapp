5ccdb4863fde05527bbf7b81042e739d712bd141
juan <juanvalen15@gmail.com>
2025-04-17 14:59:30 -0500

    replace the match-case with if conditions to allow the add task to add a task by writing it next to the command ADD

--- main.py
+++ main.py
@@ -6,58 +6,57 @@ while True:
     user_action = input(user_prompt)
     user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input(todo_prompt) + "\n"
+    if 'add' in user_action:
+        todo = user_action[4:]
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            todos.append(todo)
+        todos.append(todo)
 
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('files/todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case 'show':
-            with open('files/todos.txt') as file:
-                todos = file.readlines()
+    if 'show' in user_action:
+        with open('files/todos.txt') as file:
+            todos = file.readlines()
 
-            for list_index, item in enumerate(todos):
-                item = item.strip('\n')
-                row = f"{list_index + 1}-{item}"
-                print(row)
-        case 'edit':
-            number = int(input("Number of the todo to edit: "))
-            number = number - 1
+        for list_index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{list_index + 1}-{item}"
+            print(row)
 
-            with open('files/todos.txt','r') as file:
-                todos = file.readlines()
+    if 'edit' in user_action:
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
 
-            new_todo = input(todo_prompt)
-            todos[number] = new_todo + '\n'
+        with open('files/todos.txt','r') as file:
+            todos = file.readlines()
 
-            with open('files/todos.txt','w') as file:
-                file.writelines(todos)
+        new_todo = input(todo_prompt)
+        todos[number] = new_todo + '\n'
 
-        case 'complete':
-            number = int(input("Number of the todo to : "))
-            index  = number - 1
+        with open('files/todos.txt','w') as file:
+            file.writelines(todos)
 
-            with open('files/todos.txt','r') as file:
-                todos = file.readlines()
+    if 'complete' in user_action:
+        number = int(input("Number of the todo to : "))
+        index  = number - 1
 
-            todo_to_remove = todos[index]
-            todos.pop(index)
+        with open('files/todos.txt','r') as file:
+            todos = file.readlines()
 
-            with open('files/todos.txt','w') as file:
-                file.writelines(todos)
+        todo_to_remove = todos[index]
+        todos.pop(index)
 
-            print(f"Todo {todo_to_remove.strip('\n')} was removed from the list.")
+        with open('files/todos.txt','w') as file:
+            file.writelines(todos)
 
-        case 'exit':
-            break
-        case _:
-            print("Incorrect command!")
+        print(f"Todo {todo_to_remove.strip('\n')} was removed from the list.")
+
+    if 'exit' in user_action:
+        break
+
 
 print('Bye!')
 
